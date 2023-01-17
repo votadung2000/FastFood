fe6e49f3551aa4ff50efc7974862419d499ce68c
Vo Ta Dung <47474729+votadung2000@users.noreply.github.com>
2023-01-17 10:32:39 +0700

    fix action app

--- App.js
+++ App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import {NotifierWrapper} from 'react-native-notifier';
 
-import {Context} from './src/context';
+import store, {Context} from './src/context';
 import AppContainer from './src/modules/AppContainer';
-import store from './src/context';
 
 const App = () => {
   return (
