dec38baefbbee34f6178e8cf4f6eab85acc1587c
votadung <dung.vo@vtl-vtl.com>
2024-03-13 17:03:13 +0700

    fix error font

--- metro.config.js
+++ metro.config.js
@@ -11,6 +11,7 @@ module.exports = (async () => {
     resolver: {
       assetExts: assetExts.filter(ext => ext !== 'svg'),
       sourceExts: [...sourceExts, 'svg'],
+      assets: ['./assets/fonts/'],
     },
   };
 })();
