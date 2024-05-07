cb56016babc1b1285b5a77e91016f1ae294466c8
votadung <dung.vo@vtl-vtl.com>
2024-05-07 16:56:28 +0700

    add loading for MyWeb component

--- src/components/MyWeb.js
+++ src/components/MyWeb.js
@@ -1,22 +1,42 @@
-import React from 'react';
-import {StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, View} from 'react-native';
 import {WebView} from 'react-native-webview';
 
+import Loading from './Loading';
+
 const MyWeb = ({uri}) => {
+  const [isLoading, setLoading] = useState(true);
+
+  const openModalLoading = () => {
+    setLoading(true);
+  };
+
+  const closeModalLoading = () => {
+    setLoading(false);
+  };
+
   return (
-    <WebView
-      useWebKit
-      originWhitelist={['*']}
-      bounces={false}
-      showsVerticalScrollIndicator={false}
-      source={{uri}}
-      style={styles.container}
-    />
+    <View style={styles.container}>
+      {isLoading && <Loading />}
+      <WebView
+        useWebKit
+        originWhitelist={['*']}
+        bounces={false}
+        showsVerticalScrollIndicator={false}
+        source={{uri}}
+        style={styles.stWebView}
+        onLoadStart={openModalLoading}
+        onLoadEnd={closeModalLoading}
+      />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+  },
+  stWebView: {
     flex: 1,
   },
 });
